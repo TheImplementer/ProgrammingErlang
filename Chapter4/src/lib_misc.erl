7f405c2b97ef12d87335472a8c569d4934321939
Enrico Pelizzon <enrico.pelizzon@gmail.com>
2014-05-01 00:43:58 +0100

    added my_time_function and my_date_string functions

--- Chapter4/src/lib_misc.erl
+++ Chapter4/src/lib_misc.erl
@@ -1,9 +1,19 @@
 -module(lib_misc).
--export([my_tuple_to_list/1]).
+-export([my_tuple_to_list/1, my_time_function/1, my_date_string/0]).
 -import(erlang, [delete_element/2]).
-
 
 my_tuple_to_list({}) ->
   [];
 my_tuple_to_list(Tuple) when is_tuple(Tuple) ->
   [element(1, Tuple) | my_tuple_to_list(delete_element(1, Tuple))].
+
+my_time_function(F) ->
+  {_, _, StartingMicro} = now(),
+  F(),
+  {_, _, EndingMicro} = now(),
+  EndingMicro - StartingMicro.
+
+my_date_string() ->
+  {Year, Month, Day} = date(),
+  {Hours, Minutes, Seconds} = time(),
+  io:format("~p/~p/~p ~p:~p:~p~n", [Year, Month, Day, Hours, Minutes, Seconds]).
