82801555a913bb84b00ec1d80a769301778bcab2
Enrico Pelizzon <enrico.pelizzon@gmail.com>
2014-05-01 01:30:17 +0100

    added two different implementations of split function

--- Chapter4/src/math_functions.erl
+++ Chapter4/src/math_functions.erl
@@ -1,5 +1,5 @@
 -module(math_functions).
--export([even/1, odd/1, filter/2]).
+-export([even/1, odd/1, filter/2, split_accumulator/1, split_filter/1]).
 
 even(Number) ->
   case Number rem 2 of
@@ -15,8 +15,20 @@ odd(Number) ->
 
 filter(_, []) ->
   [];
-filter(F, [H|T]) ->
+filter(F, [H | T]) ->
   case F(H) of
-    true -> [H|filter(F, T)];
+    true -> [H | filter(F, T)];
     false -> filter(F, T)
   end.
+
+split_accumulator(L) -> split_helper(L, [], []).
+
+split_helper([], Evens, Odds) ->
+  {Evens, Odds};
+split_helper([H | T], Evens, Odds) ->
+  case even(H) of
+    true -> split_helper(T, [H | Evens], Odds);
+    false -> split_helper(T, Evens, [H | Odds])
+  end.
+
+split_filter(L) -> {filter(fun even/1, L), filter(fun odd/1, L)}.
--- Chapter4/src/math_functions_tests.erl
+++ Chapter4/src/math_functions_tests.erl
@@ -1,6 +1,6 @@
 -module(math_functions_tests).
--export([test_even/0, test_odd/0]).
--import(math_functions, [even/1, odd/1]).
+-export([test_even/0, test_odd/0, test_split_accumulator/0, test_split_filter/0]).
+-import(math_functions, [even/1, odd/1, split_accumulator/1, split_filter/1]).
 
 test_even() ->
   true = even(2),
@@ -12,4 +12,18 @@ test_odd() ->
   true = odd(3),
   false = odd(2),
   false = odd(0),
+  ok.
+
+test_split_accumulator() ->
+  {[6, 4, 2], [5, 3, 1]} = split_accumulator([1, 2, 3, 4, 5, 6]),
+  {[6, 4, 2], []} = split_accumulator([2, 4, 6]),
+  {[], [5, 3, 1]} = split_accumulator([1, 3, 5]),
+  {[], []} = split_accumulator([]),
+  ok.
+
+test_split_filter() ->
+  {[2, 4, 6], [1, 3, 5]} = split_filter([1, 2, 3, 4, 5, 6]),
+  {[2, 4, 6], []} = split_filter([2, 4, 6]),
+  {[], [1, 3, 5]} = split_filter([1, 3, 5]),
+  {[], []} = split_filter([]),
   ok.
